0ed6efa183961f77bbfcdcb5f7ea6bb37bf3440d
Logan Cole <emtlogancole@gmail.com>
2023-02-15 10:16:14 -0600

    feature: student

--- lib/student.rb
+++ lib/student.rb
@@ -1,1 +1,17 @@
+class Student
+  attr_reader :name, :age, :scores
+  def initialize(student_attributes)
+    @name = student_attributes[:name]
+    @age = student_attributes[:age]
+    @scores = student_attributes[:scores]
+    @scores = []
+  end
 
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    @scores.sum / @scores.size.to_f
+  end
+end

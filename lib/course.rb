a0b6e624648eba3af02ac8fcfb55cd9f0a317ae1
Logan Cole <emtlogancole@gmail.com>
2023-02-15 10:30:32 -0600

    feat: course

--- lib/course.rb
+++ lib/course.rb
@@ -1,1 +1,20 @@
+require_relative 'student'
 
+class Course
+  attr_reader :name, :capacity, :students
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = students
+    @students = []
+  end
+
+  def enroll(student)
+    @students << student
+  end
+
+  def full?
+    @students.count >= @capacity
+  end
+
+end
